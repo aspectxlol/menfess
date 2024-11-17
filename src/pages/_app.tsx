0175e05f8facaae8a9ffac54c3d018efece5abfa
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-11-17 22:17:44 +0700

    added seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,11 +7,19 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
   <>
       <Head>
-        <title>Menfess</title>
-        <meta name="description" content="A simple, secure, and anonymous way to send and receive messages."/>
-        <link rel="icon" href="/favicon.ico"/>
+          <title>Menfess</title>
+          <meta name="title" content="Menfess"/>
+          <meta name="description" content="A Website to Send messages Anonymously and Securely"/>
+          <meta name="keywords" content="Anonymous, Message, Confess, Secure"/>
+          <meta name="robots" content="index, follow"/>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+          <meta name="language" content="English"/>
+
+          <meta name="author" content="Louie"/>
+          <meta name="og:title" content="Menfess"/>
+          <meta name="og:description" content="A Website to Send messages Anonymously and Securely"/>
       </Head>
-      <Toaster position={'bottom-right'} toastOptions={{ duration: 1500 }}/>
+      <Toaster position={'bottom-right'} toastOptions={{duration: 1500}}/>
       <Component {...pageProps} />
   </>
   );
